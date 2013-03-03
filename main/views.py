fe0922749311b84e82a5e3e5244f2634ce9e3804
Kevin <kevin@kevin-virtual-machine.(none)>
2013-03-03 16:39:59 -0500

    changed urls

--- main/views.py
+++ main/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.shortcuts import render_to_response, get_object_or_404
+from django.template import RequestContext
+
+def index(request):
+    return render_to_response('index.html', {
+    }, context_instance=RequestContext(request))
