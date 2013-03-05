e9bdad7c17acfd3fff7288b5e54465bd99f44b5b
Kevin <kevin@kevin-virtual-machine.(none)>
2013-03-04 22:23:37 -0500

    added admin site

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.db.models import permalink
 
-# Create your models here.
+class Appointment(models.Model):
+    firstName = models.CharField(max_length=100)
+    lastName = models.SlugField(max_length=100, unique=True)
+    dateScheduled = models.DateTimeField()
+    posted = models.DateTimeField(db_index=True, auto_now_add=True)
+
+    def __unicode__(self):
+        return '%s %s' % self.firstName, self.lastName
+
+#    @permalink
+#    def get_absolute_url(self):
+#        return ('view_blog_post', None, { 'slug': self.slug })
--- main/views.py
+++ main/views.py
@@ -3,4 +3,5 @@ from django.template import RequestContext
 
 def index(request):
     return render_to_response('index.html', {
+        'appointments': Appointment.objects.all(),
     }, context_instance=RequestContext(request))
