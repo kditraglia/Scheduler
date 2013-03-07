2d605278d58bc6ee9d55dea82f4ab0540a8f2eb6
Kevin <kevin@kevin-virtual-machine.(none)>
2013-03-06 23:25:06 -0500

    make it so you cannot refresh and re-send the POST

--- main/views.py
+++ main/views.py
@@ -1,6 +1,8 @@
 from main.models import Appointment
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse 
 
 def index(request):
 
@@ -11,7 +13,7 @@ def index(request):
            dateScheduled=request.POST['Date'],
            problem=request.POST['problem'],)
         appointment.save()
-
+        return HttpResponseRedirect(reverse("main.views.index"))
     return render_to_response('index.html', {
         'appointments': Appointment.objects.all(),
     }, context_instance=RequestContext(request))
